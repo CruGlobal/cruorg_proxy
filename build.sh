612f6a6b502e481e6646e62723836bbbd417c7e2
Josh Starcher <josh.starcher@gmail.com>
2017-03-13 11:05:26 -0400

    Fix the right build file and delete the other one.

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker build -t 056154071827.dkr.ecr.us-east-1.amazonaws.com/$PROJECT_NAME:$GIT_COMMIT-$BUILD_NUMBER ./main
+docker build \
+ --build-args RESOLVER=10.10.10.38 \
+  -t 056154071827.dkr.ecr.us-east-1.amazonaws.com/$PROJECT_NAME:$GIT_COMMIT-$BUILD_NUMBER ./main
 
