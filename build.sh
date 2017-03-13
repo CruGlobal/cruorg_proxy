f72744b7a9f7fdee79818331b1dcc5206c070ad8
Josh Starcher <josh.starcher@gmail.com>
2017-03-13 11:08:43 -0400

    Fix typo

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker build \
- --build-args RESOLVER=10.10.10.38 \
+ --build-arg RESOLVER=10.10.10.38 \
   -t 056154071827.dkr.ecr.us-east-1.amazonaws.com/$PROJECT_NAME:$GIT_COMMIT-$BUILD_NUMBER ./main
 
