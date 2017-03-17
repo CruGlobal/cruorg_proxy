ed630c426b2f70975da2950a5f7306bb13b01649
Josh Starcher <josh.starcher@gmail.com>
2017-03-17 08:43:09 -0400

    Fix build arguments.

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 docker build \
- --build-arg \
- RESOLVER=10.10.10.38,AEM_LB=stage-cru-org-613775129.us-east-1.elb.amazonaws.com,DOMAIN=stage.cru.org \
+ --build-arg RESOLVER=10.10.10.38 \
+ --build-arg AEM_LB=stage-cru-org-613775129.us-east-1.elb.amazonaws.com \
+ --build-arg DOMAIN=stage.cru.org \
   -t 056154071827.dkr.ecr.us-east-1.amazonaws.com/$PROJECT_NAME:$GIT_COMMIT-$BUILD_NUMBER ./main
 
