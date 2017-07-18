b799a4a7432810a026e2b8053bc7db9096b95c51
Josh Starcher <josh.starcher@gmail.com>
2017-07-18 09:01:30 -0600

    Add upstream for storylines.

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ docker build \
  --build-arg AEM_ADDR=$AEM_ADDR \
  --build-arg DOMAIN=$DOMAIN \
  --build-arg S3_ADDR=$S3_ADDR \
+ --build-arg STORYLINES_ADDR=$STORYLINES_ADDR \
   -t 056154071827.dkr.ecr.us-east-1.amazonaws.com/$PROJECT_NAME:$GIT_COMMIT-$BUILD_NUMBER ./main
 
