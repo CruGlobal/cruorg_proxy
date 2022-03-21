08b1651389a545ffc81a4518254469d7fd6826d3
Brian Zoetewey <brian.zoetewey@cru.org>
2022-03-21 16:24:25 -0400

    Update build for GitHub Actions

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build \
+docker buildx build $DOCKER_ARGS \
  --build-arg RESOLVER="10.16.2.22 10.16.3.22" \
  --build-arg AEM_ADDR=$AEM_ADDR \
  --build-arg DOMAIN=$DOMAIN \
@@ -8,4 +8,4 @@ docker build \
  --build-arg STORYLINES_ADDR=$STORYLINES_ADDR \
  --build-arg REDIS_PORT_6379_TCP_ADDR_A=$REDIS_PORT_6379_TCP_ADDR_A \
  --build-arg REDIS_PORT_6379_TCP_ADDR_PS=$REDIS_PORT_6379_TCP_ADDR_PS \
-  -t 056154071827.dkr.ecr.us-east-1.amazonaws.com/$PROJECT_NAME:$ENVIRONMENT-$BUILD_NUMBER ./main
+ ./main
